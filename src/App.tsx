1f8e4402f6ee78410087bdeec769d98faf0eeac0
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2022-12-19 14:21:09 -0300

    Scroll top on change url

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import Shop from './pages/Shop'
+import Cart from './pages/Cart'
+import Error from './pages/Error'
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/shop" element={<Navigate to={'/shop/1'} replace />} />
+      <Route path="/shop/:pagination" element={<Shop />} />
+      <Route path="/shop/:pagination/:category" element={<Shop />} />
+      <Route path="/shop/:pagination/:category/:specific" element={<Shop />} />
+      <Route path="/products/:category/:id" element={<Products />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,59 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { ItemProvider } from './context/ItemsContext'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import './index.css'
-import Home from './pages/Home'
-import About from './pages/About'
-import Cart from './pages/Cart'
-import Error from './pages/Error'
-import { ItemProvider } from './context/ItemsContext'
-import Shop from './pages/Shop'
-import Products from './pages/Products'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/about',
-    element: <About />,
-  },
-  {
-    path: '/shop',
-    element: <Navigate to={'/shop/1'} replace />,
-  },
-  {
-    path: '/shop/:pagination',
-    element: <Shop />,
-  },
-  {
-    path: '/shop/:pagination/:category',
-    element: <Shop />,
-  },
-  {
-    path: '/shop/:pagination/:category/:specific',
-    element: <Shop />,
-  },
-  {
-    path: '/products/:category/:id',
-    element: <Products />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
-  },
-  {
-    path: '*',
-    element: <Error />,
-  },
-])
+import App from './App'
+import ScrollToTop from './helpers/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ItemProvider>
-      <RouterProvider router={router} />
+      <Router>
+        <ScrollToTop />
+        <App />
+      </Router>
     </ItemProvider>
   </React.StrictMode>
 )
