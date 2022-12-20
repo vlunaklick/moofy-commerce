f0c5538f5937bdaea11200455718d01aa2081827
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2022-12-20 11:01:50 -0300

    Cart quantity in nav

--- src/components/app/Nav.tsx
+++ src/components/app/Nav.tsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom'
 import { CgMenu, CgShoppingCart } from 'react-icons/cg'
+import { useContext } from 'react'
 
+import { ItemsContext } from '../../context/ItemsContext'
 import { useMenu } from '../../hooks/useMenu'
 
 function Nav() {
+  const { cart } = useContext(ItemsContext)
+
   const { isOpen, handleOpen, close, ref } = useMenu()
+
+  const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0)
 
   return (
     <nav
@@ -27,27 +33,28 @@ function Nav() {
           />
         </div>
       </div>
-      <div>
-        <ul className="hidden md:flex gap-24 text-zinc-900 font-medium">
-          <li className="cursor-pointer group">
-            <Link to={'/about'}>
-              About
-              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-zinc-900"></span>
-            </Link>
-          </li>
-          <li className="cursor-pointer group">
-            <Link to={'/shop/1'}>
-              Shop
-              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-zinc-900"></span>
-            </Link>
-          </li>
-          <li className="cursor-pointer">
-            <Link to={'/cart'}>
-              <CgShoppingCart className="inline-block text-2xl" />
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <ul className="hidden md:flex gap-24 text-zinc-900 font-medium">
+        <li className="cursor-pointer group">
+          <Link to={'/about'}>
+            About
+            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-zinc-900"></span>
+          </Link>
+        </li>
+        <li className="cursor-pointer group">
+          <Link to={'/shop/1'}>
+            Shop
+            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-zinc-900"></span>
+          </Link>
+        </li>
+        <li className="cursor-pointer">
+          <Link to={'/cart'} className="flex justify-between w-full relative">
+            <CgShoppingCart className="inline-block text-2xl" />
+            <span className="text-[8px] w-4 rounded-full bg-zinc-500 text-white aspect-square flex items-center justify-center absolute -right-[5px] -top-1">
+              {totalItems}
+            </span>
+          </Link>
+        </li>
+      </ul>
       {isOpen && (
         <ul className="flex flex-col md:hidden text-lg text-center absolute bg-zinc-50 w-full top-full z-50 ">
           <Link to={'/about'}>
@@ -65,10 +72,13 @@ function Nav() {
           </Link>
           <Link to={'/cart'}>
             <li
-              className="p-3 cursor-pointer border-zinc-100 border-t-2"
+              className="p-3 cursor-pointer border-zinc-100 border-t-2 flex items-center gap-1 w-full justify-center"
               onClick={() => close()}
             >
               <CgShoppingCart className="inline-block text-2xl" />
+              <span className="text-[9px] w-5 h-5 rounded-full bg-zinc-500 text-white aspect-square flex items-center justify-center">
+                {totalItems}
+              </span>
             </li>
           </Link>
         </ul>
