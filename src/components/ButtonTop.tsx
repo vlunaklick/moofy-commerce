234afc23a41b9a4ca9dda7b879ef8ac170dcf60c
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2022-12-24 16:28:45 -0300

    Refactor scale on button to top

--- src/components/ButtonTop.tsx
+++ src/components/ButtonTop.tsx
@@ -23,12 +23,11 @@ const ButtonTop = () => {
   return (
     <motion.button
       onClick={handleScrollToTop}
-      // Bounce effect while button is visible on screen, stop fade out when visible
       animate={view ? 'visible' : 'hidden'}
       variants={{
         visible: {
           opacity: 1,
-          y: 0,
+          scale: 1,
           transition: {
             duration: 0.3,
             yoyo: Infinity,
@@ -37,7 +36,7 @@ const ButtonTop = () => {
         },
         hidden: {
           opacity: 0,
-          y: 20,
+          scale: 0,
           transition: {
             duration: 0.3,
             ease: 'easeInOut',
